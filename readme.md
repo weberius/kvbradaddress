d24647df87eccdcb8f411bba6ec1eedf34905717
Wolfram Eberius <wolfram@Wolframs-MBP.fritz.box>
2016-01-11 05:24:41 +0100

    first commit

---
+++ readme.md
@@ -0,0 +1,3 @@
+# KVB Rad Addresses
+
+Ziel dieses Projektes ist mit Hilfe eines Geocoders für jede abgespeicherte Position eines Rades eine Adresse vorzuhalten und ggf. bei mehrfacher Adresse diese zu zählen.
---
+++ src/main/java/de/illilli/opendata/service/kvbradaddress/Service.java
@@ -0,0 +1,21 @@
+package de.illilli.opendata.service.kvbradaddress;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+
+import org.apache.log4j.Logger;
+
+@Path("/")
+public class Service {
+
+	private final static Logger logger = Logger.getLogger(Service.class);
+
+	@Context
+	private HttpServletRequest request;
+
+	@Context
+	private HttpServletResponse response;
+
+}
